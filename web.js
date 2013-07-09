365f8be04b80885fa0d3ea52f4e36ff6a9449748
Bhaskar Dua <bhaskar_dua@yahoo.com>
2013-07-09 16:09:18 +0000

    web.js now reads data from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var sendString = buf.toString(fs.readFileSync('./index.html'))
+  var sendString = (fs.readFileSync('./index.html')).toString();
   response.send(sendString);
 });
 
