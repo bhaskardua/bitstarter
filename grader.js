a4314d97118b140de603deac1ff59176142863fc
Bhaskar Dua <bhaskar_dua@yahoo.com>
2013-07-10 16:59:35 +0000

    Modified grader.js to accept URL arguments. Added restler npm

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,21 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfile, isUrl) {
+    if (isUrl) {
+	    return cheerio.load(htmlfile);
+    }
+    else {
+	    return cheerio.load(fs.readFileSync(htmlfile));
+    }
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, isUrl) {
+    $ = cheerioHtmlFile(htmlfile, isUrl);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,8 +71,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <url>', 'URL to check')
+	.parse(process.argv);
+    var checkJson;
+    if (program.file) {checkJson = checkHtmlFile(program.file, program.checks, false)}
+    if (program.url) {rest.get(program.url).on('complete', function(data) {setCheckJson(data);});}
+    var setCheckJson = function(data) {
+	checkJson = checkHtmlFile(data, program.checks, true);
+    };
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
